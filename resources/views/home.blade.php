b0c2638eb2c062d3e78dfd55b40af4c5b12693fc
Sébastien Lavoie <s.lavoie.b@gmail.com>
2018-09-03 16:54:47 -0400

    Slight improvement to SEO

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Translit</title>
+        <title>Translit | Live music vizualizations from your files or microphone</title>
         <script type="text/javascript" src="js/dist/lodash.js"></script>
         <script type="text/javascript" src="js/dist/butterchurn.min.js"></script>
         <script type="text/javascript" src="js/dist/butterchurnPresets.min.js"></script>
         <script type="text/javascript" src="js/dist/butterchurnPresetsExtra.min.js"></script>
-        {{-- <script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous"></script> --}}
         <script defer src="js/dist/fontawesome-v520.js"></script>
 
 
